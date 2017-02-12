87540665f5c1ae4b5182eb4472d69fd780290557
HaiderMalik <haidermalik504@gmail.com>
2017-02-12 15:14:19 +0500

    Init Repo

---
+++ src/app/products/product-list/product-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: 'product-list.component.html',
+  styleUrls: ['product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  products  = [
+    {id: 1, name:'Mobile'},
+    {id: 2, name:'Shirt'},
+  ];
+
+}
